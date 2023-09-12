dadfbd3dd484137a63c5e97e3bc65fcd8e269e47
4ART1ST <94847194+4ART1ST@users.noreply.github.com>
2023-09-12 15:35:42 +0800

    Add files via upload

---
+++ p3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class p3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {// 注意，如果输入是多个测试用例，请通过while循环处理多个测试用例
+            String a = in.next();
+            String b = in.next();
+            String[] aa = a.split(";");
+            System.out.println();
+        }
+    }
+}
